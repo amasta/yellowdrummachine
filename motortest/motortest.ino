40c62eff21867fb5bb18fbc30d4830b826a3050d
theDecompiler <ryan.lubbecke@gmail.com>
2012-12-21 17:30:07 -0500

    newer

--- motortest/motortest.ino
+++ motortest/motortest.ino
@@ -14,23 +14,37 @@
 #define MOTOR_US 3
 #define MOTOR_US_FREQ MOTOR12_64KHZ
 #define MOTOR_US_SPEED 255
-#define PIN_US_TRIG 28
-#define PIN_US_ECHO 29
+#define PIN_US_TRIG 48
+#define PIN_US_ECHO 49
+
+// bass motor constants
+#define MOTOR_ARM_BASS 4
+#define MOTOR_ARM_BASS_FREQ MOTOR12_64KHZ
+#define MOTOR_ARM_BASS_SPEED 255
+
+
 
 
 AF_DCMotor motorTreadRight(MOTOR_TREAD_RIGHT, MOTOR_TREAD_RIGHT_FREQ);
 AF_DCMotor motorTreadLeft(MOTOR_TREAD_LEFT, MOTOR_TREAD_LEFT_FREQ);
-AF_DCMotor motorUS(MOTOR_US, MOTOR12_64KHZ);
- 
+AF_DCMotor motorUS(MOTOR_US, MOTOR_US_FREQ);
+AF_DCMotor motorArmBass(MOTOR_ARM_BASS, MOTOR_ARM_BASS_FREQ);
+
+
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(115200);
   
-  //Serial.println("SETUP CONFIGURATION");
+  Serial.println("SETUP CONFIGURATION");
+  
   
   motorTreadRight.setSpeed(MOTOR_TREAD_RIGHT_SPEED);
   motorTreadLeft.setSpeed(MOTOR_TREAD_LEFT_SPEED);
   motorUS.setSpeed(MOTOR_US_SPEED);
+  motorArmBass.setSpeed(MOTOR_ARM_BASS_SPEED);
+
+  pinMode(PIN_US_TRIG, OUTPUT);
+  pinMode(PIN_US_ECHO, INPUT);
 }
 
 void driveForward(int timeLength) {
@@ -82,7 +96,7 @@ void driveForwardRight(int timeLength) {
 }
 
 void driveForwardLeft(int timeLength) {
-	motorTreadLeft.run(BACKWARD);
+	motorTreadLeft.run(FORWARD);
 	
 	delay(timeLength);
 	
@@ -105,8 +119,103 @@ void driveBackwardLeft(int timeLength) {
 	motorTreadLeft.run(RELEASE);
 }
 
+void playBass()
+{
+  motorArmBass.run(BACKWARD);
+  delay(100);
+  motorArmBass.run(RELEASE); 
+}
 
+float getUSDistance() {
+  digitalWrite(PIN_US_TRIG, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(PIN_US_TRIG, HIGH);
+  //delayMicroseconds(10);
+  
+  digitalWrite(PIN_US_TRIG, LOW);
+
+  return pulseIn(PIN_US_ECHO, HIGH) / 58;  
+}
+
+float getUSDistanceRight() {
+  motorUS.run(FORWARD);
+  delay(500);
+  
+  float distance = getUSDistance();
+  motorUS.run(RELEASE);
+  
+  return distance;
+}
+
+float getUSDistanceLeft() {
+  motorUS.run(BACKWARD);
+  delay(500);
+ 
+  float distance = getUSDistance();
+  motorUS.run(RELEASE);
+  
+  return distance;
+}
 
 void loop() {
+  float distanceLeft = 0;
+  float distanceForward = 0;
+  float distanceRight = 0;
+  
+  distanceForward = getUSDistance();
+  distanceLeft = getUSDistanceLeft();
+  distanceRight = getUSDistanceRight();
+  
+  Serial.print("L: ");
+  Serial.println(distanceLeft);
+  delay(500);
+  Serial.print("F: ");
+  Serial.println(distanceForward);
+  Serial.print("R: ");
+  Serial.println(distanceRight);
+  
+  if (distanceForward > 10)
+  {
+    driveForward(2000);
+  }
+ else if (distanceLeft > distanceRight)
+ {
+   driveBackward(4000);
+   driveRotateLeft(1400);
+ } 
+ else if (distanceLeft < distanceRight)
+ {
+   driveBackward(4000);
+   driveRotateRight(1400);
+ }
+    
+  
+  /*
+  playBass();
+  delay(200);
+  
+  playBass();
+  delay(20);
+  playBass();
+  delay(20);
+  playBass();
+  delay(20);
+  
+  playBass();
+  delay(300);
+  playBass();
+  delay(200);
+  playBass();
+  delay(1000);
 
+
+  driveForward(2000);
+  driveRotateRight(4000);
+  driveRotateLeft(4000);
+  driveBackward(2000);
+  driveForwardRight(2000);
+  driveBackwardRight(2000);
+  driveForwardLeft(2000);
+  driveBackwardLeft(2000);*/
 }
